11d12196466ec4678274b124485fdb28b23d4f40
andiua <anduhust@ukr.net>
2018-05-19 17:21:57 +0300

    my first-dyplom

---
+++ js/js.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+  $('.slider-wrap').slick({
+  	  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+   arrows: false,
+  });
+});
+   function initMap() {
+        var uluru = {lat: 50.4890686, lng: 30.4752407};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 15,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
